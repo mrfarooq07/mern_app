fbfab4858f8397d77e533055e867c43b5ec164bd
Mr-Farooq-599 <127822114+Mr-Farooq-599@users.noreply.github.com>
2023-06-09 18:56:59 +0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("First API call hit");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const port = process.env.PORT || 8888;
+
+app.listen(8888, console.log(`server started on port ${port}`));
